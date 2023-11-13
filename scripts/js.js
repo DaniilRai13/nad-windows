56e4d70192606eefacc09f6bbdbb4b9470062b63
DaniilRai13 <testnet02@mail.ru>
2023-11-13 17:38:35 +0300

    feat: created func for contact form. Used firebase

--- scripts/js.js
+++ scripts/js.js
@@ -1,22 +1,44 @@
+const form = document.querySelector('form')
+const inputForName = form.querySelector('.form-name')
+const inputForPhone = form.querySelector('.form-phone')
+const btn = form.querySelector('.form-btn')
 
-gsap.fromTo('.main', { opacity: 1 }, {
-    opacity: 0,
-    scrollTrigger: {
-        trigger: ".main",
-        start: "center",
-        end: 'bottom',
-        scrub: true
+let userClass = new User()
+
+inputForName.addEventListener('input', () => {
+    console.log(isValid(inputForName.value, inputForPhone.value))
+    btn.disabled = !isValid(inputForName.value, inputForPhone.value)
+})
+
+inputForPhone.addEventListener('input', () => {
+    console.log(isValid(inputForName.value, inputForPhone.value))
+    btn.disabled = !isValid(inputForName.value, inputForPhone.value)
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (isValid(inputForName.value, inputForPhone.value)) {
+        console.log(inputForName.value, inputForPhone.value)
+        const userInfo = {
+            name: inputForName.value.trim(),
+            phone: inputForPhone.value.trim(),
+            date: new Date().toJSON(),
+        }
+        // btn.disabled = true
+        userClass.create(userInfo).then(() => {
+                inputForName.value = inputForPhone.value = ''
+                btn.disabled = true
+            })
     }
 })
 
-const anchor = document.querySelector("a[href*='#slider']")
-anchor.addEventListener('click', (e) => {
-    e.preventDefault()
-    const blockId = anchor.getAttribute('href')
-    document.querySelector('' + blockId).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    })
-})
+function isValid(name, phone) {
+    return (name.length < 20 && name.length >= 2) && (phone.length <= 13 && phone.length >= 9)
+}
+
+
+
+
+
 
 
