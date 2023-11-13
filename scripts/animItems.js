854a7f9d6b4f7918bc4bf6ccc6f04480280dde0e
DaniilRai13 <testnet02@mail.ru>
2023-11-13 17:40:33 +0300

    feat: used gsap lib for animation

--- scripts/animItems.js
+++ scripts/animItems.js
@@ -1,3 +1,23 @@
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+
+if (ScrollTrigger.isTouch !== 1) {
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.3,
+        effects: true
+    })
+
+    // gsap.fromTo('.footer .form__inner ', { x: -50, opacity: 0 }, {
+    //     x: 0,
+    //     opacity: 1,
+    //     scrollTrigger: {
+    //         trigger: '.form__inner',
+    //         scrub: true,
+    //     }
+    // })
+}
+
 const animItems = document.querySelectorAll('.anim-items')
 
 if (animItems.length > 0) {
@@ -45,3 +65,34 @@ if (animItems.length > 0) {
 //     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
 
 // }
+
+// const anchor = document.querySelector("a[href*='#slider']")
+const anchor = document.querySelector(".contacts")
+const anchors = document.querySelectorAll("a[href*='#']")
+
+
+anchors.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault()
+        const blockId = item.getAttribute('href')
+        document.querySelector('' + blockId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
+
+
+anchor.addEventListener('click', (e) => {
+    // e.preventDefault()
+    console.log(e.target)
+    const block = document.querySelector("body")
+    window.scrollTo({ top: block.offsetHeight, behavior: "smooth" })
+})
+
+
+console.log(document.querySelector('#contacts').getBoundingClientRect().top, anchor)
+
+
+
+
