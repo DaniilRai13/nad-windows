c0b87a858e5fe6e22037c7f4ad414a1aa4e171f3
DaniilRai13 <testnet02@mail.ru>
2024-01-26 17:18:30 +0300

    feat: create site

--- scripts/animItems.js
+++ scripts/animItems.js
@@ -1,23 +1,3 @@
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
-
-if (ScrollTrigger.isTouch !== 1) {
-    ScrollSmoother.create({
-        wrapper: '.wrapper',
-        content: '.content',
-        smooth: 1.3,
-        effects: true
-    })
-
-    // gsap.fromTo('.footer .form__inner ', { x: -50, opacity: 0 }, {
-    //     x: 0,
-    //     opacity: 1,
-    //     scrollTrigger: {
-    //         trigger: '.form__inner',
-    //         scrub: true,
-    //     }
-    // })
-}
-
 const animItems = document.querySelectorAll('.anim-items')
 
 if (animItems.length > 0) {
@@ -57,41 +37,6 @@ if (animItems.length > 0) {
         animOnScroll()
     }, 300)
 }
-
-// function offset(el) {
-//     const rect = el.getBoundingClientRect(),
-//         scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-//         scrollTop = window.scrollY || document.documentElement.scrollTop;
-//     return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-
-// }
-
-// const anchor = document.querySelector("a[href*='#slider']")
-const anchor = document.querySelector(".contacts")
-const anchors = document.querySelectorAll("a[href*='#']")
-
-
-anchors.forEach((item, i) => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault()
-        const blockId = item.getAttribute('href')
-        document.querySelector('' + blockId).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-    })
-})
-
-
-anchor.addEventListener('click', (e) => {
-    // e.preventDefault()
-    console.log(e.target)
-    const block = document.querySelector("body")
-    window.scrollTo({ top: block.offsetHeight, behavior: "smooth" })
-})
-
-
-console.log(document.querySelector('#contacts').getBoundingClientRect().top, anchor)
 
 
 
--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -1,37 +1,92 @@
-class User {
-    async create(user) {
-        const res = await fetch('https://nad-windows-default-rtdb.firebaseio.com/users.json', {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'applicatin/json'
-            }
-        })
-        const data = res.json()
-        return console.log(data)
+const TELEGRAM_BOT_TOKEN = '6978457392:AAEgu54EszfEobhrm5MeSD8dTbUEj7MO1h8';
+const TELEGRAM_CHAT_ID = '@NAD_app_bot1';
+const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+const form = document.querySelector('form')
+const inputForName = form.querySelector('.form-name')
+const inputForPhone = form.querySelector('.form-phone')
+const btn = form.querySelector('.form-btn')
+
+inputForName.addEventListener('input', () => {
+    btn.disabled = !isValid(inputForName.value, inputForPhone.value)
+})
+
+inputForPhone.addEventListener('input', () => {
+    btn.disabled = !isValid(inputForName.value, inputForPhone.value)
+    console.log(inputForPhone.value.length)
+})
+
+inputForPhone.addEventListener('focus', (e) => {
+    if (!(e.target.value.length > 4 && e.target.value.slice(0, 4) === '+375')) {
+        e.target.value = '+375'
     }
+
+})
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    let { username, userphone } = Object.fromEntries(new FormData(e.target).entries())
+
+    // First of all we create new FormData
+    //after that we make array of arrays[key,value] and then we make object of our array[](fromEntries)
+
+
+    try {
+        let applicationNumber = 0
+
+        await setApplicationNumber(username, userphone)
+        await countApplications().then(data => applicationNumber = data)
+
+        let applicationText = `Заявка номер ${applicationNumber} \nИмя: ${username} \nНомер телефона: ${userphone}`
+        console.log(applicationText)
+        // form.reset()
+        let response = await fetch(API, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text: applicationText,
+            })
+        });
+
+        if (response.ok) {
+            form.reset()
+            btn.disabled = true
+        } else {
+            throw new Error(response.statusText)
+        }
+    } catch (error) {
+        console.log(error)
+        console.log('Application text was not sent ')
+    }
+
+})
+
+function isValid(name, phone) {
+    return (name.length < 20 && name.length >= 2) && (phone.length == 13)
 }
 
-// const mysql = require('mysql2');
+async function setApplicationNumber(name, phone) {
+    return await fetch('https://nad-windows-default-rtdb.firebaseio.com/users.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            data: new Date(Date.now()),
+            name,
+            phone,
+        })
+    });
+}
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'nadwindo_test',
-//     database: 'nadwindo_danik',
-//     password: 'Dimalox12!'
-// })
-
-// connection.connect((err) => {
-//     if (err) console.log(err)
-//     else console.log('Database conn.....')
-// })
-
-
-// const contactForm = document.querySelector('#contacts')
-// const btn = contactForm.querySelector('.form-btn')
-
-// btn.addEventListener('click', async (e) => {
-//     e.preventDefault()
-//     console.log(12)
-// })
+async function countApplications() {
+    return await fetch('https://nad-windows-default-rtdb.firebaseio.com/users.json')
+        .then(res => res.json())
+        .then(data => {
+            return Object.keys(data).length === null ? 0 : Object.keys(data).length
+        })
+}
